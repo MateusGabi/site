f75a804d889b114f0d944f3440264a718d9ed71f
Mateus Gabi <mateusgabimoreira@hotmail.com>
2016-04-10 19:38:37 -0400

    versão 2.7 da ameda.js

--- pass/js/engine.js
+++ pass/js/engine.js
@@ -1,65 +1,34 @@
+var version = "2.7";
+
 /*
- 
- Versão 2.2.4 
- 
+
  @author Mateus Gabi    [@matgabi17 on Twitter]
  @author Renan Benatti  [@renanbdias on Twitter]
  
  */
 
-//  'a', 'b', 'c' e 'd' são variáveis do tipo 'integer' e recebem valores sorteados;
-
-// var a = criarUnico();
-// var b = criarUnico();
-// var c = criarUnico();
-// var d = criarUnico();
-
 //  Valor limite de prints de palavras, ou seja, a Ameda printará entre 0 e 7 palavras;
 var e = 4;
 
-//  Função que retorna um número aleatório
-// function criarUnico()
-// {
-
-//     var sorteados = [];
-//     var valorMaximo = 8;
-
-//     if (sorteados.length == valorMaximo)
-//     {
-//         if (confirm('#'))
-//             sorteados = [];
-//         else
-//             return;
-//     }
-
-//     var sugestao = Math.ceil(Math.random() * valorMaximo);
-
-//     while (sorteados.indexOf(sugestao) >= 0)
-//         sugestao = Math.ceil(Math.random() * valorMaximo);
-
-//     sorteados.push(sugestao);
-
-//     return sugestao;
-// }
-
+// Vetor que armazena as palavras
 var w = [
     //[Susbstantivo Comum], [Substantivo Próprio], [adjetivo], [advérbio], [verbo]
-    "amor", "Amanda", "amigável", "ante-ontem",
-    "banana", "Bruna", "baixinho", "bastante",
-    "coração", "Carla", "carinhosa", "carinhosamente",
-    "dedo", "Diego", "devagar",
-    "esquerdo", "Eduardo", "esquerdo", "estudar",
-    "faca", "Fabiana", "fácil", "facilmente", "ficar",
-    "goiabada", "Goiás", "gostosa", "gritar",
-    "homem", "Hungria", "hoje",
-    "igreja", "Iara", "ignorante",
-    "joaninha", "João", "jurássico", "jamais",
+    "amor", "Amanda", "amigável", "ante-ontem", "abano",
+    "banana", "Bruna", "baixinho", "bastante", "boliche",
+    "chuchu", "coração", "Carla", "carinhosa", "carinhosamente",
+    "dedo", "Diego", "devagar", "divindade",
+    "espelho", "esquerdo", "Eduardo", "estudar",
+    "faca", "Fabiana", "fácil", "facilmente", "ficar", "festança",
+    "gigante", "goiabada", "Goiás", "gostosa", "gritar",
+    "homem", "na Hungria", "hoje", "hospital",
+    "indubitável", "igreja", "Iara", "ignorante",   
+    "jato", "joaninha", "João", "jurássico", "jamais",
     "Keila",
-    "limão", "Luísa", "lituano",
-    "maneta", "Mateus", "memorável", "muito",
+    "limão", "Luísa", "lituano", "liquidificar",
+    "namorar", "maneta", "Mateus", "memorável", "muito",
     "navio", "Naiara", "nunca", "natal", "nascer",
     "oligarquia", "Osório",
-    "patinho", "Pedro", "preto",
+    "patinho", "Pedro", "de preto",
     "queijo",
     "rua", "Raíssa", "ruiva",
     "salada", "Silvia",
@@ -69,25 +38,28 @@ var w = [
     "Wellington",
     "xadrez", "Xico",
     "Ygritte",
-    "zebra",
-    "abano",
-    "boliche",
-    "chuchu",
-    "divindade",
-    "espelho",
-    "festança",
-    "gigante",
-    "hospital",
-    "indubitável",
-    "jato",
-    "liquidificar",
-    "namorar"
+    "zebra"  
 ];
 
-function printarFrase() {
+// em construção! terá dois parâmetros: um inteiro 'i' e um vetor de inteiros 'array'
+function contains() {
+    return false;
+    
+    // pseudocode
+    
+    // se i está em Array
+    //     return true
+    // senão
+    //     return false
+    
+    // obs: a implementação deste código implicará na mudança do getPhrasal()
+}
+
+function getPhrasal() {
 
     var retorno = "";
 
+    // vetor de inteiros que representa os índices de w pertencentes ao retorno;
     var f = -1;
 
     for (i = 1; i <= e; i++) {
@@ -97,20 +69,18 @@ function printarFrase() {
         // função para encontrar elemento no vetor
         // f será o indice do vetor w
 
-        var _f = parseInt(Math.random() * w.length);
-
-        if (f == _f)
+        var _f = parseInt(Math.random() * w.length);        
+        
+        var b = false;
+        if (f == _f){
             i--;
-        else
+        }
+        else{
             f = _f;
+            b = true;
+        }
 
-        // if (f > w.length || f < 0) {
-        //     do nothing
-        // } else {
-        //     retorno += " " + w[f];
-        // }
-
-        if (f > 0 && f < w.length)
+        if (b)
             retorno += " " + w[f]
 
     }
@@ -118,30 +88,21 @@ function printarFrase() {
     return document.getElementById('frase').innerHTML = "<input id=\"ph\" class=\"form-control input-lg\" type=\"text\" placeholder=\"Carregando...\" value=\"" + retorno + "\" readonly>";
 }
 
-// Resultado Inteiro em Trilhões
+// Resultado Inteiro em Milhões
+function getStatus() {
+    var p = 1;
 
-function printarWordsStatus() {
-    var p = 0;
-
-    for (var i = 1; i <= e; i++)
-        p += Math.pow(w.length, i);
+// 87 * 86 * 85 * 84
+    for (var i = 0; i < e; i++)
+        p = p * (w.length - i);
 
     p = parseInt(p / Math.pow(10, 6));
 
-    // p = parseInt(p);
-
-    return document.getElementById("statusWord").innerHTML = "Nós temos " + w.length + " palavras disponíveis em " + p + " milhões de combinações distintas!";
+     return "Nós temos <b>" + w.length + " Palavras</b> disponíveis em mais de <b>" + p + " Milhões</b> de combinações distintas!";
+    
 }
 
-// We don't need it
-
-// function combinations()
-// {
-
-//     var p = 0;
-
-//     for (var i = 1; i <= e; i++)
-//         p += Math.pow(w.length, i);
-
-//     return p;
-// }
+// Versão atual da Ameda
+function getVersion() {
+    return version;
+}
